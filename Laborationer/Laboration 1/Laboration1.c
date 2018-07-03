d3fd2c8a5ab468ac9f75473151c286a5663b5e67
darkraven92 <36090181+darkraven92@users.noreply.github.com>
2018-07-03 23:53:44 +0200

    new code

--- Laborationer/Laboration 1/Laboration1.c
+++ Laborationer/Laboration 1/Laboration1.c
@@ -1,7 +1,33 @@
 #include <stdio.h>
 int main()
 {
-   // printf() displays the string inside quotation
-   printf("Hello, World!");
-   return 0;
+    int v,vt = -25;
+
+    printf("Lunar decent challenge!\n");
+    printf("You will pilot a lunar decent the last 250m.\n");
+    printf("Each turn represent 1-second decent time\n");
+    printf("Set the throttle for each turn (0-100)\n");
+    printf("Time\tHeight\tVelocity\tThrottle\n");
+
+    for (int f = 0; f <= 100; ++f){
+                v = vt+(0.1*f-1.5);
+
+
+    }
+    printf(v);
+
+    return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
